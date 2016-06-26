5a91542302679ea22b54646799583debaeb517f5
Salman808 <salmananjum808@gmail.com>
2016-06-26 20:12:03 +0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+a = input(" Enter Num 1 : " )
+b =input(" Enter num 2 : " )
+c =raw_input(" Enter Operation you e=want to perform : ")
+if c=='+':
+ print 'Sum of Num 1 and Num 2 : ', a+b
+elif c=='-':  
+ print 'Difference of num 1 and num 2  : ', a-b
+elif c=='*':
+ print  'Product of Num 1 and Num 2  : ', a*b
+elif c=='/':
+ print 'Divisor of Num 1 and Num 2  : ', a/b
+elif c=='%':
+ print 'Remainder after Num1 / num2  : ' , a%b
+else :
+ print ' you Enterend invalid operation\n '
